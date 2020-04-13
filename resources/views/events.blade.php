872b3ab16f2c0a44ae99354d806e48ef75d591d0
Terrel Stephen <terrel.stephen@dcmail.ca>
2020-04-13 11:42:03 -0700

    Placeholder Entry forms
    Need to validate all the required info that neds to be entered to create a new even or worker, etc

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts/layout')
 @section('content')
+    <h1>Create Event</h1>
+    <form>
+        <label for="eventName">Event Name:</label><br>
+        <input type="text" id ="eventName" name ="eventName"><br>
+        <label for="trackDate">Track Date:</label><br>
+        <input type="date" id ="trackdate" name ="trackdate"><br>
+        <label for="eventTrack"> Event Track: </label><br>
+        <input type="text" id ="eventTrack" name ="eventTrack"><br><br>
+        <button type="button"> Add New Event </button>   
+    </form>
+
     <h1>Events</h1>
     <table class="table table-bordered" id="table">
         <thead>
