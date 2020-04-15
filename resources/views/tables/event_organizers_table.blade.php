34a82fd40eafd6da3e64e80a8c875d7b846cf072
Colin552 <colin-strong@hotmail.com>
2020-04-15 08:45:46 -0400

    Fixed form columns

--- resources/views/tables/event_organizers_table.blade.php
+++ resources/views/tables/event_organizers_table.blade.php
@@ -3,8 +3,9 @@
        <tr>
           <th>ID</th>
           <th>Name</th>
-          <th>Date</th>
-          <th>Track</th>
+          <th>Contact First</th>
+          <th>Contact Last</th>
+          <th>Contact Phone</th>
        </tr>
     </thead>
     <tbody>
