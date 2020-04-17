9a82ac3bebd6f37049077ba124dc002667feae1e
Colin552 <colin-strong@hotmail.com>
2020-04-17 11:58:04 -0400

    Imported external libraries

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -8,10 +8,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
 
-    <script src="//code.jquery.com/jquery-1.12.3.js"></script>
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.css">
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.js"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="{{ asset('jquery-1.12.3.js')}}"></script>
+    <link rel="stylesheet" type="text/css" href="{{asset('jquery.dataTables.css')}}">
+    <script type="text/javascript" charset="utf8" src="{{asset('jquery.dataTables.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('bootstrap.min.css')}}" >
     <title>Document</title>
 </head>
 <body>
