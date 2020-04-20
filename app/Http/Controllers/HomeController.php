a1794e44e9a8ea6e76435bccd8f50d8c30ef6e97
Terrel Stephen <terrel.stephen@dcmail.ca>
2020-04-20 11:31:03 -0700

    Continuing Home page work

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Events;
 
 class HomeController extends Controller
 {
     public function show() {
-        return view('home');
+
+        $upcomingEvents = \App\Event::where('event_date', '>', \DB::raw('NOW()'))->orderBy('event_date', 'asc')->get();
+        //$events = DB::table('events')->get();
+        return view('home', compact(['upcomingEvents']));
+        //return view('home', ['events'=> $events]);
+        //return view('home');
     }
+
+
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,27 @@
 @extends('layouts/layout')
 @section('content')
+
+<div class="col-md-6 card">
+		    <h4>Upcoming Events</h4>
+		    <table class="table">
+		    	<tr>
+		    		<th>Event ID</th>
+		    		<th>Event Name</th>
+		    		<th>Event Date</th>
+		    		<th>Event Track</th>
+                    <th>Workers Required<th>
+                    <th>Workers Registered<th>
+                    <th>Selection Email Sent?<th>
+		    	</tr>
+                @foreach($upcomingEvents as $event)
+		    		<tr>
+		    			<td>{{ $event->event_id }}</td>
+		    			<td>{{ $event->event_name }}</td>
+		    			<td>{{ $event->event_date }}</td>
+		    			<td><a href="/events/track/{{$event->event_track}}">{{$event->event_track}}</a></td>
+                        <td>{{ $event->event_workers_needed }}</td>
+		       		</tr>
+		    	@endforeach
+		    </table>
+		</div>
 @endsection
