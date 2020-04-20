82cfa920fc7478b946e934f4dd89d527d73f3e24
Colin552 <colin-strong@hotmail.com>
2020-04-20 11:02:20 -0400

    Fixed event and worker Update/Delete

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -17,16 +17,16 @@ class EventController extends Controller
 
     public function showUpcoming()
     {
-        $upcomingEvents = \App\Event::where('event_date', '>', \DB::raw('NOW()'))->orderBy('event_date', 'asc')->get();
-        $pastEvents = \App\Event::where('event_date', '<=', \DB::raw('NOW()'))->orderBy('event_date', 'desc')->get();
+        $upcomingEvents = \App\Event::where('event_date', '>', DB::raw('NOW()'))->orderBy('event_date', 'asc')->get();
+        $pastEvents = \App\Event::where('event_date', '<=', DB::raw('NOW()'))->orderBy('event_date', 'desc')->get();
 
         return view('events_report', compact(['upcomingEvents', 'pastEvents']));
     }
 
     public function showTrack($track)
     {
-        $upcomingEvents = \App\Event::where('event_track', $track)->where('event_date', '>', \DB::raw('NOW()'))->orderBy('event_date', 'asc')->get();
-        $pastEvents = \App\Event::where('event_track', $track)->where('event_date', '<=', \DB::raw('NOW()'))->orderBy('event_date', 'desc')->get();
+        $upcomingEvents = \App\Event::where('event_track', $track)->where('event_date', '>', DB::raw('NOW()'))->orderBy('event_date', 'asc')->get();
+        $pastEvents = \App\Event::where('event_track', $track)->where('event_date', '<=', DB::raw('NOW()'))->orderBy('event_date', 'desc')->get();
 
         return view('track', compact(['track', 'upcomingEvents', 'pastEvents']));
     }
@@ -70,27 +70,31 @@ class EventController extends Controller
 
     public function updateEvent(Request $request, $id){
 
-        $validatedData = $request->validate([
+        $request->validate([
             'eventName' => 'required|max:75',
             'eventDate' => 'required',
             'eventTrack' => 'required|max:45',
-            'event_workers_needed' => 'max:11',
+            'eventWorkers' => 'numeric|max:11',
         ]);
 
+        $event = Event::find($id);
+        
         $event->event_name = $request->get('eventName');
         $event->event_date = $request->get('eventDate');
         $event->event_track = $request->get('eventTrack');
         $event->organizer_id_for_event = $request->get('organizerID');
-        $event->event_workers_needed = $request->get('event_workers_needed');
+        $event->event_workers_needed = $request->get('eventWorkers');
 
         $event->save();
+        
         return redirect('/events/input');
     }
     
-    public function deleteWorker($id){
+    public function deleteEvent($id){
 
         $event = Event::where('event_id',$id)->first();
         if($event != null){
+            DB::table('worker_event_registrations')->where('worker_event_registration_event_id', $id)->delete();
             $event->delete();
             return redirect('/events/input');
         }
